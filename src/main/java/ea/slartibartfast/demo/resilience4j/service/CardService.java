2ff355319bbf76d579cd78631c6d3ecd09603ff1
Erdem Akdoğan <erdem.akdogan@iyzico.com>
2021-12-03 14:48:17 +0300

    adds retry for cardApi operations

--- src/main/java/ea/slartibartfast/demo/resilience4j/service/CardService.java
+++ src/main/java/ea/slartibartfast/demo/resilience4j/service/CardService.java
@@ -5,6 +5,7 @@ import ea.slartibartfast.demo.resilience4j.model.CardVo;
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,24 +22,31 @@ public class CardService {
 
     @CircuitBreaker(name = CARD_API, fallbackMethod = "retrieveCardCircuitBreakerFallback")
     @Bulkhead(name = CARD_API)
+    @Retry(name = CARD_API, fallbackMethod = "retrieveCardRetryFallback")
     public CardVo retrieveCard(final String cardToken) {
         log.info("going to retrieve card for cardToken {}", cardToken);
         return cardApiClient.retrieveCard(cardToken);
     }
 
-    public CardVo retrieveCardCircuitBreakerFallback(Throwable t) {
-        log.warn("Card api not healthy! Returning empty card", t);
+    public CardVo retrieveCardCircuitBreakerFallback(CallNotPermittedException e) {
+        log.warn("Card api not healthy! Returning empty card");
         return CardVo.builder().build();
+    }
+
+    public CardVo retrieveCardRetryFallback(Exception e) throws Exception {
+        log.warn("Card api not healthy! Retry failed");
+        throw e;
     }
 
     @CircuitBreaker(name = CARD_API, fallbackMethod = "saveCardCircuitBreakerFallback")
     @Bulkhead(name = CARD_API)
+    @Retry(name = CARD_API, fallbackMethod = "saveCardRetryFallback")
     public String saveCard(CardVo cardVo) {
         log.info("card {} will be saved", cardVo.getCardNumber());
         return cardApiClient.saveCard(cardVo);
     }
 
-    public String saveCardCircuitBreakerFallback(Throwable e) {
+    public String saveCardCircuitBreakerFallback(Exception e) {
         log.warn("Card api not healthy! Card cannot be saved, returning empty card token");
         return EMPTY_CARD_TOKEN;
     }
@@ -46,5 +54,10 @@ public class CardService {
     public String saveCardCircuitBreakerFallback(CallNotPermittedException e) {
         log.warn("Card api not healthy! Call not permitted");
         return null;
+    }
+
+    public String saveCardRetryFallback(Exception e) throws Exception {
+        log.warn("Card api not healthy! Retry failed");
+        throw e;
     }
 }
